18095b2f0a44bd69b37dec45395e8440a2ca2b33
Skyler Lauren <skylerlauren@Skylers-MacBook-Pro.local>
2015-05-03 15:50:01 -0500

    Initial Commit

---
+++ SKATMXParser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SKATMXParser
+//
+//  Created by Skyler Lauren on 5/3/15.
+//  Copyright (c) 2015 Sprite Kit Alliance. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
